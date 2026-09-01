593fdf4b84094d442021633be72f952ddb26ee4e
Gabriel Gonçalves 👨🏻‍💻 <gabrielgmamorim@gmail.com>
2026-09-01 16:53:55 -0300

    fix: eliminei código conflitante e repetido

--- app/vagas/[id]/not-found.tsx
+++ app/vagas/[id]/not-found.tsx
@@ -1,11 +1,3 @@
- main
-export default function NotFoundVaga() {
-  return (
-    <div>
-      <h2>Vaga não encontrada</h2>
-      <p>Ela pode ter sido encerrada ou o link está errado.</p>
-    </div>
-
 import Link from "next/link";
 
 export default function NotFound() {
